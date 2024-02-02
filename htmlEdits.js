6673ba417f2685d4431bac2c542f48d617a70fe8
Infinite <111289762+InfiniteSmasher@users.noreply.github.com>
2024-02-02 20:06:25 +0400

    Update htmlEdits.js

--- htmlEdits.js
+++ htmlEdits.js
@@ -17,4 +17,4 @@ equipTemplate.innerHTML = equipTemplate.innerHTML.replace(
 	`categoryLocKey] }} ({{ equip.showingItems.length }})`
 );
 
-window.betterInventoryUpdateSync = 1;
+window["betterInventoryUpdateSync"] = 1;
