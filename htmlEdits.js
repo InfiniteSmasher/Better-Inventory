d51d5516e8bed95af6276cf47528702b07bafa15
Infinite <111289762+InfiniteSmasher@users.noreply.github.com>
2024-02-02 20:34:34 +0400

    Update htmlEdits.js

--- htmlEdits.js
+++ htmlEdits.js
@@ -2,19 +2,22 @@ let equipTemplate = document.getElementById("equip-screen-template");
 equipTemplate.innerHTML = equipTemplate.innerHTML.replace(
 	`" v-on:keyup="onItemSearchChange" class="ss_field font-nunito box_relative fullwidth">`,
 	`" v-on:keyup="onItemSearchChange" class="ss_field font-nunito roundme_lg box_relative" :class="{'limited-input' : isOnEquipModeFeatured}">
- 	<button id="randomize-button" onclick="randomizeSkin()" v-show="isEquipModeInventory" class="ss_button roundme_lg btn_blue bevel_blue btn-account-w-icon random-button">
+	<button id="randomize-button" onclick="randomizeSkin()" v-show="isEquipModeInventory" class="ss_button roundme_lg btn_blue bevel_blue btn-account-w-icon random-button">
 		<i class="fas fa-random"></i>
 	</button>`
 );
 
 equipTemplate.innerHTML = equipTemplate.innerHTML.replace(
-	`class="item-search-wrap box_relative"`, 
+	`class="item-search-wrap box_relative"`,
 	`class="item-search-wrap box_relative" :class="{'inventory-skin-search' : isEquipModeInventory, 'shop-skin-search' : isOnEquipModeSkins || isOnEquipModeFeatured}"`
 );
 
 equipTemplate.innerHTML = equipTemplate.innerHTML.replace(
-	`categoryLocKey] }}`, 
+	`categoryLocKey] }}`,
 	`categoryLocKey] }} ({{ equip.showingItems.length }})`
 );
 
-window["betterInventoryUpdateSync"] = 1;
+let betterInventoryUpdateCheck = document.createElement('div');
+betterInventoryUpdateCheck.id = "betterInventoryUpdateCheck";
+betterInventoryUpdateCheck.dataset.version = 1;
+document.head.appendChild(betterInventoryUpdateCheck);
