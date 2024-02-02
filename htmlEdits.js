9a83669d8743d51b604868513bd17392904c6117
Infinite <111289762+InfiniteSmasher@users.noreply.github.com>
2024-02-02 19:42:56 +0400

    Update htmlEdits.js
    Added inventory item count and version variable.

--- htmlEdits.js
+++ htmlEdits.js
@@ -4,10 +4,17 @@ equipTemplate.innerHTML = equipTemplate.innerHTML.replace(
 	`" v-on:keyup="onItemSearchChange" class="ss_field font-nunito roundme_lg box_relative" :class="{'limited-input' : isOnEquipModeFeatured}">
  	<button id="randomize-button" onclick="randomizeSkin()" v-show="isEquipModeInventory" class="ss_button roundme_lg btn_blue bevel_blue btn-account-w-icon random-button">
 		<i class="fas fa-random"></i>
-	</button>
-`);
+	</button>`
+);
 
 equipTemplate.innerHTML = equipTemplate.innerHTML.replace(
 	`class="item-search-wrap box_relative"`, 
-	`class="item-search-wrap box_relative" :class="{'inventory-skin-search' : isEquipModeInventory, 'shop-skin-search' : isOnEquipModeSkins || isOnEquipModeFeatured}"
-`);
+	`class="item-search-wrap box_relative" :class="{'inventory-skin-search' : isEquipModeInventory, 'shop-skin-search' : isOnEquipModeSkins || isOnEquipModeFeatured}"`
+);
+
+equipTemplate.innerHTML = equipTemplate.innerHTML.replace(
+	`categoryLocKey] }}`, 
+	`categoryLocKey] }} ({{ equip.showingItems.length }})`
+);
+
+window.betterInventoryUpdateSync = 1;
