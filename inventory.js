56421a618c5434ed49e15185137db61d1b85ef56
Infinite <111289762+InfiniteSmasher@users.noreply.github.com>
2024-02-02 20:03:45 +0400

    Update inventory.js

--- inventory.js
+++ inventory.js
@@ -317,6 +317,8 @@ fetch("https://cdn.jsdelivr.net/gh/InfiniteSmasher/Better-Inventory@latest/modDa
 		makeVueChanges();
 	});
 
+/*
 if (typeof(document.betterInventoryUpdateSync) == "undefined" || document.betterInventoryUpdateSync != 1) {
 	alert("Hello Gamer!\n\nIt looks like your version of Better Inventory isn't updated to the latest version.\n\nTo ensure that you receive future updates with all the latest features, go to Tampermonkey settings and make sure that the \"Update Interval\" setting under the \"Externals\" category is set to \"Always\".\n\nThanks for using Better Inventory, enjoy the added features!\n- Infinite Smasher :)");
 }
+*/
